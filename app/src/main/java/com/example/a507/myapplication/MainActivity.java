114fc0f659fb1f3ea6b09df5abc8451d2da021e8
HEOSUMIN <gjtnals79@naver.com>
2018-10-02 16:49:59 +0900

    go Dial, Call, SmS

--- app/src/main/java/com/example/a507/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a507/myapplication/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.a507.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    protected Button btHomepage,btDial,btCall,btSms;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btHomepage =(Button)findViewById(R.id.btHomepage);
+        btHomepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("htp://ice.mokwon.ac.kr"));
+                startActivity(intent);
+            }
+        });
+
+        btDial =(Button)findViewById(R.id.btDial);
+        btDial.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0428297670"));
+                startActivity(intent);
+            }
+        });
+
+        btCall=(Button)findViewById(R.id.btCall);
+        btCall.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:0428297670"));
+                startActivity(intent);
+            }
+        });
+
+        btSms = (Button)findViewById(R.id.btSms);
+        btSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:0428297670"));
+                intent.putExtra("sms_body", "Mokwon University");
+                startActivity(intent);
+            }
+        });
+
     }
 }
